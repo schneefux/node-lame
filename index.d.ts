8276a70999d0e6144dcfb271cbc74d35cb38a7c4
schneefux <schneefux+commit@schneefux.xyz>
2020-06-14 20:29:12 +0200

    Update typings, fixes import

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,40 @@
-declare module 'lame' {
-    import { WriteStream } from 'fs';
-    import { DuplexOptions } from 'stream';
+import { WriteStream } from 'fs';
+import { DuplexOptions } from 'stream';
 
-    export interface DecoderOptions extends DuplexOptions {
-        readonly decoder: string;
-    }
+export interface DecoderOptions extends DuplexOptions {
+    readonly decoder: string;
+}
 
-    export interface EncoderOptions extends DuplexOptions {
-        readonly float;
-        readonly signed?: number;
-        readonly bitDepth?: number;
-        readonly channels?: number;
-        readonly sampleRate?: number;
-    }
+export interface EncoderOptions extends DuplexOptions {
+    readonly float;
+    readonly signed?: number;
+    readonly bitDepth?: number;
+    readonly channels?: number;
+    readonly sampleRate?: number;
+}
 
-    /**
-     * The `Decoder` accepts an MP3 file and outputs raw PCM data.
-     * 
-     * @param opts Configurations.
-     * @returns A writable stream.
-     */
-    export function Decoder(opts?: DecoderOptions): WriteStream;
+/**
+ * The `Decoder` accepts an MP3 file and outputs raw PCM data.
+ * 
+ * @param opts Configurations.
+ * @returns A writable stream.
+ */
+export function Decoder(opts?: DecoderOptions): WriteStream;
 
-    /**
-     * The `Encoder` accepts raw PCM data and outputs an MP3 file.
-     * 
-     * @param opts Configurations.
-     * @returns A writable stream.
-     */
-    export function Encoder(opts?: EncoderOptions): WriteStream;
+/**
+ * The `Encoder` accepts raw PCM data and outputs an MP3 file.
+ * 
+ * @param opts Configurations.
+ * @returns A writable stream.
+ */
+export function Encoder(opts?: EncoderOptions): WriteStream;
 
-    /*
-     * Channel Modes
-     */
-    export enum ChannelModes {
-        STEREO,
-        JOINTSTEREO,
-        DUALCHANNEL,
-        MONO
-    }
+/*
+ * Channel Modes
+ */
+export enum ChannelModes {
+    STEREO,
+    JOINTSTEREO,
+    DUALCHANNEL,
+    MONO
 }
